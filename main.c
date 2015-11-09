e7311cbd5913f7c81ae1fed4f65d8ff92041875f
Sven Steudte <sven.steudte@gmail.com>
2015-11-09 19:37:36 +0100

    Implemented VGA resolution

--- main.c
+++ main.c
@@ -13,6 +13,35 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+/*
+	Takes an VGA image of OV9655 camera and encodes it in realtime to JPEG.
+	The image has a capture time of 264ms.
+	For sampling a picture PA0 must be pulled high. (User button on STM32F429 discovery board)
+	The picture is then transmitted by serial interface on PD5 (38k2 8n1).
+	PG13 is blinking (Green LED on STM32F429 discovery board) when the uC is
+	ideling. So when the picture has been transmitted completely PG13 is
+	blinking.
+
+	This example does not need any external SDRAM. It just uses it's internal
+	SRAM. So this example should also work on the STM32F407 discovery board.
+
+	The inital code came from http://mikrocontroller.bplaced.net/wordpress/?page_id=1115
+
+	The camera must be connected to
+
+	PA4=DCMI_HSYNC -> HSNYC (or HREF)
+	PA6=DCMI_PCLK  -> PCLK
+	PB6=DCMI_D5    -> D5
+	PB7=DCMI_VSYNC -> VSYNC
+	PC6=DCMI_D0    -> D0
+	PC7=DCMI_D1    -> D1
+	PC8=DCMI_D2    -> D2
+	PC9=DCMI_D3    -> D3
+	PE4=DCMI_D4    -> D4
+	PE5=DCMI_D6    -> D6
+	PE6=DCMI_D7    -> D7
+	RET and PWDN can be left unconnected
+*/
 
 #include "ch.h"
 #include "hal.h"
@@ -32,9 +61,10 @@
 #define TRANSFER_IT_ENABLE_MASK (uint32_t)(DMA_SxCR_TCIE | DMA_SxCR_HTIE | \
                                            DMA_SxCR_TEIE | DMA_SxCR_DMEIE)
 
-#define OV9655_MAXX		320
-#define OV9655_MAXY		240
-#define OV9655_BUFFER	OV9655_MAXX*16
+#define OV9655_MAXX			640
+#define OV9655_MAXY			480
+#define OV9655_BUFFER_SIZE	OV9655_MAXX*16
+#define OV9655_BUFFER_NUM	5
 
 #define OV9655_DCMI_DMA_CHANNEL		DMA_Channel_1
 #define OV9655_DCMI_BASE_ADR		((uint32_t)0x50050000)
@@ -44,8 +74,9 @@
 #define  OV9655_I2C_ADR        0x30  /* Slave-address vom OV9655 */
 
 
-uint16_t ov9655_ram_buffer[OV9655_BUFFER];
-uint16_t ov9655_ram_buffer2[OV9655_BUFFER];
+uint16_t ov9655_ram_buffer[OV9655_BUFFER_SIZE];
+uint16_t ov9655_ram_buffer2[OV9655_BUFFER_NUM][OV9655_BUFFER_SIZE];
+volatile uint32_t buffNum = 0;
 
 CACHE_ALIGN BGR RGB16x16[16][16];
 CACHE_ALIGN color_t R[16][16], G[16][16], B[16][16];
@@ -54,7 +85,7 @@ CACHE_ALIGN conv Cb8x8[8][8];
 CACHE_ALIGN conv Cr8x8[8][8];
 uint64_t subsclk = 0;
 uint64_t readclk = 0;
-static uint8_t jpeg[30*1024];
+static uint8_t jpeg[35*1024];
 static uint32_t jpeg_pos = 0;
 
 uint8_t rxbuf[1];
@@ -62,51 +93,51 @@ uint8_t rxbuf[1];
 // I2C interface #2
 static const I2CConfig i2cfg2 = {
     OPMODE_I2C,
-    400000,
+    200000,
     FAST_DUTY_CYCLE_2,
 };
 
+// I2C camera configuration for VGA resolution
 static uint8_t OV9655_CONFIG[]=
 {
-0x00,0x00,0x01,0x80,0x02,0x80,0x03,0x02,0x04,0x03,0x09,0x01, // <= I2C camera for QVGA
-0x0b,0x57,0x0e,0x61,0x0f,0x40,0x11,0x01,0x12,0x62,0x13,0xc7,
-0x14,0x3a,0x16,0x24,0x17,0x18,0x18,0x04,0x19,0x01,0x1a,0x81,
-0x1e,0x00,0x24,0x3c,0x25,0x36,0x26,0x72,0x27,0x08,0x28,0x08,
-0x29,0x15,0x2a,0x00,0x2b,0x00,0x2c,0x08,0x32,0x12,0x33,0x00,
-0x34,0x3f,0x35,0x00,0x36,0x3a,0x38,0x72,0x39,0x57,0x3a,0xcc,
-0x3b,0x04,0x3d,0x99,0x3e,0x02,0x3f,0xc1,0x40,0xc0,0x41,0x41,
-0x42,0xc0,0x43,0x0a,0x44,0xf0,0x45,0x46,0x46,0x62,0x47,0x2a,
-0x48,0x3c,0x4a,0xfc,0x4b,0xfc,0x4c,0x7f,0x4d,0x7f,0x4e,0x7f,
-0x4f,0x98,0x50,0x98,0x51,0x00,0x52,0x28,0x53,0x70,0x54,0x98,
-0x58,0x1a,0x59,0x85,0x5a,0xa9,0x5b,0x64,0x5c,0x84,0x5d,0x53,
-0x5e,0x0e,0x5f,0xf0,0x60,0xf0,0x61,0xf0,0x62,0x00,0x63,0x00,
-0x64,0x02,0x65,0x20,0x66,0x00,0x69,0x0a,0x6b,0x5a,0x6c,0x04,
-0x6d,0x55,0x6e,0x00,0x6f,0x9d,0x70,0x21,0x71,0x78,0x72,0x11,
-0x73,0x01,0x74,0x10,0x75,0x10,0x76,0x01,0x77,0x02,0x7A,0x12,
-0x7B,0x08,0x7C,0x16,0x7D,0x30,0x7E,0x5e,0x7F,0x72,0x80,0x82,
-0x81,0x8e,0x82,0x9a,0x83,0xa4,0x84,0xac,0x85,0xb8,0x86,0xc3,
-0x87,0xd6,0x88,0xe6,0x89,0xf2,0x8a,0x24,0x8c,0x80,0x90,0x7d,
-0x91,0x7b,0x9d,0x02,0x9e,0x02,0x9f,0x7a,0xa0,0x79,0xa1,0x40,
-0xa4,0x50,0xa5,0x68,0xa6,0x4a,0xa8,0xc1,0xa9,0xef,0xaa,0x92,
-0xab,0x04,0xac,0x80,0xad,0x80,0xae,0x80,0xaf,0x80,0xb2,0xf2,
-0xb3,0x20,0xb4,0x20,0xb5,0x00,0xb6,0xaf,0xb6,0xaf,0xbb,0xae,
-0xbc,0x7f,0xbd,0x7f,0xbe,0x7f,0xbf,0x7f,0xbf,0x7f,0xc0,0xaa,
-0xc1,0xc0,0xc2,0x01,0xc3,0x4e,0xc6,0x05,0xc7,0x81,0xc9,0xe0,
-0xca,0xe8,0xcb,0xf0,0xcc,0xd8,0xcd,0x93,0x12,0x63,0x40,0x10,
-0x15,0x08
+	0x00,0x00,0x01,0x80,0x02,0x80,0x03,0x02,0x04,0x03,0x09,0x01,
+	0x0b,0x57,0x0e,0x61,0x0f,0x40,0x11,0x01,0x12,0x62,0x13,0xc7,
+	0x14,0x3a,0x16,0x24,0x17,0x18,0x18,0x04,0x19,0x01,0x1a,0x81,
+	0x1e,0x00,0x24,0x3c,0x25,0x36,0x26,0x72,0x27,0x08,0x28,0x08,
+	0x29,0x15,0x2a,0x00,0x2b,0x00,0x2c,0x08,0x32,0x12,0x33,0x00,
+	0x34,0x3f,0x35,0x00,0x36,0x3a,0x38,0x72,0x39,0x57,0x3a,0xcc,
+	0x3b,0x04,0x3d,0x99,0x3e,0x02,0x3f,0xc1,0x40,0xc0,0x41,0x41,
+	0x42,0xc0,0x43,0x0a,0x44,0xf0,0x45,0x46,0x46,0x62,0x47,0x2a,
+	0x48,0x3c,0x4a,0xfc,0x4b,0xfc,0x4c,0x7f,0x4d,0x7f,0x4e,0x7f,
+	0x4f,0x98,0x50,0x98,0x51,0x00,0x52,0x28,0x53,0x70,0x54,0x98,
+	0x58,0x1a,0x59,0x85,0x5a,0xa9,0x5b,0x64,0x5c,0x84,0x5d,0x53,
+	0x5e,0x0e,0x5f,0xf0,0x60,0xf0,0x61,0xf0,0x62,0x00,0x63,0x00,
+	0x64,0x02,0x65,0x20,0x66,0x00,0x69,0x0a,0x6b,0x5a,0x6c,0x04,
+	0x6d,0x55,0x6e,0x00,0x6f,0x9d,0x70,0x21,0x71,0x78,0x72,0x01,
+	0x73,0x01,0x74,0x10,0x75,0x10,0x76,0x01,0x77,0x02,0x7A,0x12,
+	0x7B,0x08,0x7C,0x16,0x7D,0x30,0x7E,0x5e,0x7F,0x72,0x80,0x82,
+	0x81,0x8e,0x82,0x9a,0x83,0xa4,0x84,0xac,0x85,0xb8,0x86,0xc3,
+	0x87,0xd6,0x88,0xe6,0x89,0xf2,0x8a,0x24,0x8c,0x80,0x90,0x7d,
+	0x91,0x7b,0x9d,0x02,0x9e,0x02,0x9f,0x7a,0xa0,0x79,0xa1,0x40,
+	0xa4,0x50,0xa5,0x68,0xa6,0x4a,0xa8,0xc1,0xa9,0xef,0xaa,0x92,
+	0xab,0x04,0xac,0x80,0xad,0x80,0xae,0x80,0xaf,0x80,0xb2,0xf2,
+	0xb3,0x20,0xb4,0x20,0xb5,0x00,0xb6,0xaf,0xb6,0xaf,0xbb,0xae,
+	0xbc,0x7f,0xbd,0x7f,0xbe,0x7f,0xbf,0x7f,0xbf,0x7f,0xc0,0xaa,
+	0xc1,0xc0,0xc2,0x01,0xc3,0x4e,0xc6,0x05,0xc7,0x80,0xc9,0xe0,
+	0xca,0xe8,0xcb,0xf0,0xcc,0xd8,0xcd,0x93,0x12,0x63,0x40,0x10,
+	0x15,0x08
 };
 
 
-void OV9655_RAM2BMP(void);
+void OV9655_RAM2SD(void);
 void OV9655_Snapshot2RAM(void);
 void OV9655_InitDMA(void);
 void OV9655_InitDCMI(void);
-void OV9655_InitGPIO(void);
+void OV9655_InitGPIO(bool fast);
 void dma_avail(uint32_t flags);
 
-/*
-RGB to YCbCr Conversion:
-*/
+
+// RGB to YCbCr Conversion:
 // Y = 0.299*R + 0.587*G + 0.114*B
 __inline color RGB2Y(const color r, const color g, const color b)
 {
@@ -241,7 +272,7 @@ void subsample3(const color_t R[16][16], const color_t G[16][16], const color_t 
 	}
 }
 
-void write_jpeg(const unsigned char buff[], const unsigned size)
+inline void write_jpeg(const unsigned char buff[], const unsigned size)
 {
 	memcpy(&jpeg[jpeg_pos], buff, size);
 	jpeg_pos += size;
@@ -249,6 +280,7 @@ void write_jpeg(const unsigned char buff[], const unsigned size)
 
 void OV9655_Snapshot2RAM(void)
 {
+	// Add JPEG header to buffer
 	huffman_start(OV9655_MAXY & -16, OV9655_MAXX & -16);
 
 	// DCMI init
@@ -258,11 +290,60 @@ void OV9655_Snapshot2RAM(void)
 	// Capture enable
 	DCMI->CR |= (uint32_t)DCMI_CR_CAPTURE;
 
-	chThdSleepMilliseconds(2000);
+	// Encode JPEG data
+	uint32_t lines = 0;
+	while(lines < OV9655_MAXY / 16 -1) // FIXME: Somehow the STM32 only produces 29 interrupts instead of 30
+	{
+		unsigned x,xb,yb;
+		uint16_t color;
+
+		while(buffNum <= lines) // Wait for data by DMA
+			(void)0;
+
+		for (x = 0; x < OV9655_MAXX-15; x += 16)
+		{
+			// Create 16x16 block
+			for(yb=0;yb<16;yb++) {
+				for(xb=x;xb<x+16;xb++) {
+					color=ov9655_ram_buffer2[lines%OV9655_BUFFER_NUM][yb*OV9655_MAXX+xb];
+					RGB16x16[yb][xb-x].Blue = ((color&0x001F)<<3); // 5bit blue
+					RGB16x16[yb][xb-x].Green = ((color&0x07E0)>>3); // 6bit green
+					RGB16x16[yb][xb-x].Red = ((color&0xF800)>>8); // 5bit red
+				}
+			}
+
+			// Encode JPEG
+			// getting subsampled Cb and Cr
+			subsample2(RGB16x16, Y8x8, Cb8x8, Cr8x8);
+			//subsample3(R, G, B, Y8x8, Cb8x8, Cr8x8);
+
+			dct3(Y8x8[0][0], Y8x8[0][0]);	// 1 Y-transform
+			dct3(Y8x8[0][1], Y8x8[0][1]);	// 2 Y-transform
+			dct3(Y8x8[1][0], Y8x8[1][0]);	// 3 Y-transform
+			dct3(Y8x8[1][1], Y8x8[1][1]);	// 4 Y-transform
+			dct3(Cb8x8, Cb8x8);				// Cb-transform
+			dct3(Cr8x8, Cr8x8);				// Cr-transform
+
+			huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[0][0]);
+			huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[0][1]);
+			huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[1][0]);
+			huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[1][1]);
+			huffman_encode(HUFFMAN_CTX_Cb, (short*)Cb8x8);
+			huffman_encode(HUFFMAN_CTX_Cr, (short*)Cr8x8);
+		}
+
+		lines++;
+	}
+
+	// Add JPEG footer to buffer
+	chThdSleepMilliseconds(100);
 	huffman_stop();
 }
 
-void OV9655_RAM2BMP(void)
+/**
+  * Transmits JPEG encoded buffer by serial
+  */
+void OV9655_RAM2SD(void)
 {
 	uint32_t n;
 	for(n=0;n<jpeg_pos;n++) {
@@ -270,58 +351,34 @@ void OV9655_RAM2BMP(void)
 	}
 }
 
+/**
+  * ISR callback called when DCMI buffer moved into RAM by DMA completly
+  * Buffer size: OV9655_BUFFER_SIZE = OV9655_MAXX * 16 * bytes
+  * ISR moves buffer to ov9655_ram_buffer2 so DMA and does not bother JPEG encoding
+  * The data is then processed by OV9655_Snapshot2RAM()
+  */
 void dma_avail(uint32_t flags)
 {
 	(void)flags;
 
 	palSetPad(GPIOG, 14);
-
-	memcpy(&ov9655_ram_buffer2, &ov9655_ram_buffer, OV9655_BUFFER*sizeof(uint16_t));
-
-	unsigned x,xb,yb;
-	uint16_t color;
-
-	for (x = 0; x < OV9655_MAXX-15; x += 16)
-	{
-		for(yb=0;yb<16;yb++) {
-			for(xb=x;xb<x+16;xb++) {
-				color=ov9655_ram_buffer2[yb*OV9655_MAXX+xb];
-				RGB16x16[yb][xb-x].Blue = ((color&0x001F)<<3); // 5bit blue
-				RGB16x16[yb][xb-x].Green = ((color&0x07E0)>>3); // 6bit green
-				RGB16x16[yb][xb-x].Red = ((color&0xF800)>>8); // 5bit red
-			}
-		}
-
-		// getting subsampled Cb and Cr
-		subsample2(RGB16x16, Y8x8, Cb8x8, Cr8x8);
-		//subsample3(R, G, B, Y8x8, Cb8x8, Cr8x8);
-
-		dct3(Y8x8[0][0], Y8x8[0][0]);	// 1 Y-transform
-		dct3(Y8x8[0][1], Y8x8[0][1]);	// 2 Y-transform
-		dct3(Y8x8[1][0], Y8x8[1][0]);	// 3 Y-transform
-		dct3(Y8x8[1][1], Y8x8[1][1]);	// 4 Y-transform
-		dct3(Cb8x8, Cb8x8);				// Cb-transform
-		dct3(Cr8x8, Cr8x8);				// Cr-transform
-
-		huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[0][0]);
-		huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[0][1]);
-		huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[1][0]);
-		huffman_encode(HUFFMAN_CTX_Y, (short*)Y8x8[1][1]);
-		huffman_encode(HUFFMAN_CTX_Cb, (short*)Cb8x8);
-		huffman_encode(HUFFMAN_CTX_Cr, (short*)Cr8x8);
-	}
+	memcpy(&ov9655_ram_buffer2[buffNum%OV9655_BUFFER_NUM], &ov9655_ram_buffer, OV9655_BUFFER_SIZE*sizeof(uint16_t));
+	buffNum++;
 	palClearPad(GPIOG, 14);
 
-	DMA2->LIFCR = (uint32_t)(DMA_FLAG_TCIF1 & RESERVED_MASK);
+	//DMA2->LIFCR = (uint32_t)(DMA_FLAG_TCIF1 & RESERVED_MASK);
 }
 
+/**
+  * Initializes DMA
+  */
 void OV9655_InitDMA(void)
 {
     const stm32_dma_stream_t *stream = STM32_DMA2_STREAM1;
     dmaStreamAllocate(stream, 10, (stm32_dmaisr_t)dma_avail, NULL);
     dmaStreamSetPeripheral(stream, ((uint32_t*)OV9655_DCMI_REG_DR_ADDRESS));
     dmaStreamSetMemory0(stream, (uint32_t)&ov9655_ram_buffer);
-    dmaStreamSetTransactionSize(stream, OV9655_BUFFER/sizeof(uint16_t));
+    dmaStreamSetTransactionSize(stream, OV9655_BUFFER_SIZE/sizeof(uint16_t));
     dmaStreamSetMode(stream, OV9655_DCMI_DMA_CHANNEL | DMA_DIR_PeripheralToMemory | DMA_PeripheralInc_Disable |
 						DMA_MemoryInc_Enable | DMA_PeripheralDataSize_Word | DMA_MemoryDataSize_HalfWord |
 						DMA_Mode_Circular | DMA_Priority_High | DMA_MemoryBurst_Single | DMA_PeripheralBurst_Single | STM32_DMA_CR_TCIE);
@@ -329,6 +386,9 @@ void OV9655_InitDMA(void)
     dmaStreamEnable(stream);
 }
 
+/**
+  * Initializes DCMI
+  */
 void OV9655_InitDCMI(void)
 {
 	// Clock enable
@@ -342,10 +402,23 @@ void OV9655_InitDCMI(void)
 				DCMI_VSPolarity_High | DCMI_HSPolarity_High | DCMI_CaptureRate_All_Frame | DCMI_ExtendedDataMode_8b;
 }
 
-void OV9655_InitGPIO(void)
+/**
+  * Initializes GPIO (for DCMI) and setup a CLOCKOUT pin (PA8)
+  * which is needed by the camera (XCLK pin)
+  * The high speed clock supports communication by I2C (XCLK = 8MHz)
+  * The slow clock allows sampling of VGA resolution in realtime (XCLK = 2.7MHz)
+  * @param Activate fast clock
+  */
+void OV9655_InitGPIO(bool fast)
 {
 	palSetPadMode(GPIOA, 8, PAL_MODE_ALTERNATE(0)); // PA8             -> XCLK
-	RCC->CFGR = (RCC->CFGR & (uint32_t)0xF8FFFFFF) | (uint32_t)0x04000000;
+	if(fast)
+	{
+		RCC->CFGR = (RCC->CFGR & (uint32_t)0xF8FFFFFF) | (uint32_t)0x04000000;
+	} else {
+		RCC->CFGR = (RCC->CFGR & (uint32_t)0xF8FFFFFF) | (uint32_t)0x05000000;
+		RCC->CFGR = (RCC->CFGR & (uint32_t)0xFF9FFFFF) | (uint32_t)0x00400000;
+	}
 
 	palSetPadMode(GPIOA, 4, PAL_MODE_ALTERNATE(13)); // PA4=DCMI_HSYNC -> HSNYC
 	palSetPadMode(GPIOA, 6, PAL_MODE_ALTERNATE(13)); // PA6=DCMI_PCLK  -> PCLK  
@@ -360,7 +433,9 @@ void OV9655_InitGPIO(void)
 	palSetPadMode(GPIOE, 6, PAL_MODE_ALTERNATE(13)); // PE6=DCMI_D7    -> D7
 }
 
-
+/**
+  * Entry point of program
+  */
 int main(void) {
 	halInit();
 	chSysInit();
@@ -376,8 +451,8 @@ int main(void) {
 	palSetPadMode(GPIOD, 5, PAL_MODE_ALTERNATE(7));
 	palSetPadMode(GPIOD, 6, PAL_MODE_ALTERNATE(7));
 
-	// Configure GPIOs (activate camera clock)
-	OV9655_InitGPIO();
+	// Configure clock (fast clock otherwise the camera does not do I2C)
+	OV9655_InitGPIO(true);
 
 	// Send settings to OV9655
 	systime_t tmo = MS2ST(100);
@@ -389,11 +464,14 @@ int main(void) {
 		chThdSleepMilliseconds(3);
 	}
 
+	// Configure slower clock (otherwise we wont able to sample the picture in real time)
+	OV9655_InitGPIO(false);
+
 	while (true) {
 		if (palReadPad(GPIOA, 0)) {	// User button pressed
 			OV9655_InitDMA();		// Init DMA
 			OV9655_Snapshot2RAM();	// Sample data from DCMI through DMA to RAM
-			OV9655_RAM2BMP();		// Transmit image (BMP-encoded) by serial to computer
+			OV9655_RAM2SD();		// Transmit image (JPEG-encoded) by serial to computer
 		}
 
 		palTogglePad(GPIOG, 13); // Toogle LED (blinking)
